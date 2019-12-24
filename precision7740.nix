738a54d047a4f31a134d2b27f44ce478b5ba7096
Hazel Weakly <hazel@theweaklys.com>
2019-12-23 17:41:28 -0800

    Add matterhorn; initial boot logo for work laptop; tweakz4tridactyl

--- precision7740.nix
+++ precision7740.nix
@@ -1,16 +1,17 @@
 with {
-  pkgs = import ./nix {};
+  pkgs = import ./nix { };
   sources = import ./nix/sources.nix;
 }; {
-  imports = [
-    ./machines/precision7740.nix
-    ./common.nix
-  ];
+  imports = [ ./machines/precision7740.nix ./common.nix ];
 
   boot.kernelModules = [ "kvm-intel" "kvm-amd" "amdgpu" ];
+  boot.plymouth.logo = ./dots/Untitled.png;
   # boot.plymouth.logo = pkgs.fetchurl {
   #   url = "https://pbs.twimg.com/profile_images/499601444920496128/Fc5vigRB_400x400.png"
   # };
+
+  environment.systemPackages = with pkgs;
+    [ (import "${sources.mhnix}/configs.nix" { }).matterhorn ];
 
   networking.interfaces.eno1.useDHCP = true;
   networking.interfaces.wlp111s0.useDHCP = true;
