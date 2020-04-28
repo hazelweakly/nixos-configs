4fc9f66e916f23923144deb36f88494ba8ce3f69
Hazel Weakly <hazel@theweaklys.com>
2020-04-28 10:08:35 -0700

    Add srid cachix

---
+++ cachix/srid.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://srid.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
+    ];
+  };
+}
+    
