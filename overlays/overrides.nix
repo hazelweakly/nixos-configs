e52d202487aaa257c79857fb4eb9d9a6d2f661b0
Hazel Weakly <hazel@theweaklys.com>
2021-11-22 14:09:23 -0800

    large cleanup

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -1,5 +1,4 @@
 final: prev: rec {
   neovim-nightly = final.inputs.neovim-flake.packages.${prev.system}.neovim;
-  rnix-lsp = prev.callPackage ../rnix-lsp.nix { };
   mkalias = final.inputs.mkalias.packages.${prev.system}.mkalias;
 }
--- shells/README.md
+++
@@ -1,6 +0,0 @@
-# Development shells
-
-Some assorted inspiration:
-
-- <https://github.com/BenSchZA/nix-shells>
-- <https://emre.xyz/nix-integration-for-stack>
