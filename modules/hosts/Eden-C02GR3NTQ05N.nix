08bef6490661b3d0389f9b1dcb7dbb4761e376ba
Hazel Weakly <hazel@theweaklys.com>
2022-02-10 08:47:09 -0800

    fix M1 host setup

--- modules/hosts/Eden-C02GR3NTQ05N.nix
+++ modules/hosts/Eden-C02GR3NTQ05N.nix
@@ -1,9 +1,8 @@
-{ self, inputs, ... }: [
-  ./common.nix
+{ self, inputs, ... }@args: [
   {
     networking.hostName = "Eden-C02GR3NTQ05N";
     environment.etc.hostname.text = ''
       Eden-C02GR3NTQ05N
     '';
   }
-]
+] ++ (import ./common.nix args)
