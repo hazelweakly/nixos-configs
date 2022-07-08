62076f58fa37d780353934189c7e361effdf949a
Hazel Weakly <hazel@theweaklys.com>
2022-07-08 14:43:54 -0700

    update overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,4 @@
 { self, inputs, ... }: self.lib.rake ./. // {
-  rust-overlay = inputs.rust-overlay.overlay;
+  rust-overlay = inputs.rust-overlay.overlays.default;
   inputs = _: _: { inherit inputs; };
 }
