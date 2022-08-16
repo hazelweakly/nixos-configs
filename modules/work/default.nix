274578ed8bfa6f8bcdb91cb74bc9736710da4b90
Hazel Weakly <hazel@theweaklys.com>
2022-08-16 09:30:49 -0700

    stop using gpg for now

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -1,12 +1,11 @@
 { pkgs, ... }: {
-  imports = [ ./gpg.nix ];
+  # imports = [ ./gpg.nix ];
   home-manager.users.hazelweakly = { config, ... }: {
-    imports = [ ../../home/gpg.nix ];
+    # imports = [ ../../home/gpg.nix ];
     programs.git = {
-      userName = pkgs.lib.mkForce "Hazel Weakly";
-      userEmail = pkgs.lib.mkForce "hazel@theweaklys.com";
-      signing.signByDefault = true;
-      signing.key = null;
+      # userName = pkgs.lib.mkForce "Hazel Weakly";
+      # signing.signByDefault = false;
+      # signing.key = null;
       includes = [{
         condition = "gitdir:~/src/personal/";
         contents = {
