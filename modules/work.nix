ccfd27169582ff92a634f8052e32da29fa83dc6f
Hazel Weakly <hazel@theweaklys.com>
2023-01-11 13:03:37 -0800

    add gke stuff

--- modules/work.nix
+++ modules/work.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ pulumi-bin gum packer gh jq dasel figlet /* ansible */ google-cloud-sdk ];
+  environment.systemPackages = with pkgs; [
+    pulumi-bin
+    gum
+    packer
+    gh
+    jq
+    dasel
+    figlet
+    # ansible
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    kubectl
+  ];
 
   homebrew.taps = [ "equinix-labs/otel-cli" "withgraphite/tap" ];
   homebrew.brews = [ "tailscale" "lxc" "otel-cli" "withgraphite/tap/graphite" ];
