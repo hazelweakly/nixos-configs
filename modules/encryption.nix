c63b6634092369de70843591d0ed57cce255e196
Hazel Weakly <hazel@theweaklys.com>
2023-01-31 13:09:27 -0800

    secure boot for real

--- modules/encryption.nix
+++ modules/encryption.nix
@@ -3,11 +3,11 @@
 
   boot.bootspec.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.secureboot = {
-  #   enable = true;
-  #   signingKeyPath = "/etc/secureboot/keys/db/db.key";
-  #   signingCertPath = "/etc/secureboot/keys/db/db.pem";
-  # };
+  boot.loader.secureboot = {
+    enable = true;
+    signingKeyPath = "/etc/secureboot/keys/db/db.key";
+    signingCertPath = "/etc/secureboot/keys/db/db.pem";
+  };
 
   boot.initrd.luks.devices."luks-1ac9712b-5da2-4e68-b647-bd7bb9a2e6be".crypttabExtraOpts = [
     "tmp2-device=auto"
--- nixos-hosts/configuration.nix
+++ nixos-hosts/configuration.nix
@@ -31,7 +31,7 @@
   # boot.loader.systemd-boot.enable = true;
   boot.tmpOnTmpfs = true;
   boot.loader.timeout = 0;
-  boot.loader.systemd-boot.configurationLimit = 50;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
