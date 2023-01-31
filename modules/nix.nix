7463b64812b936397bb9a844d869e15dac85005b
Hazel Weakly <hazel@theweaklys.com>
2023-01-31 12:29:01 -0800

    clean up commented out file

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,15 +1,18 @@
 { pkgs, lib, inputs, self, ... }:
-with builtins;
-{
-  # services.nix-daemon.enable = true; # darwin only
-  # system.stateVersion = 4; # darwin only
-  nix.package = pkgs.nixUnstable;
-  nix.settings.experimental-features = [ "flakes" "nix-command" ];
-  nix.settings.keep-outputs = true;
-  nix.settings.keep-derivations = true;
-  nix.settings.auto-optimise-store = true;
+lib.mkMerge [
+  {
+    nix.package = pkgs.nixUnstable;
+    nix.settings.experimental-features = [ "flakes" "nix-command" ];
+    nix.settings.keep-outputs = true;
+    nix.settings.keep-derivations = true;
+    nix.settings.auto-optimise-store = true;
 
-  nix.nixPath = lib.mapAttrsToList (n: _: "${n}=/etc/nix/inputs/${n}") (self.lib.inputsWithPkgs inputs);
-  nix.registry = mapAttrs (_: v: { flake = v; }) (self.lib.inputsWithOutputs inputs);
-  environment.etc = lib.mapAttrs' (n: v: self.lib.nameValuePair "nix/inputs/${n}" { source = v.outPath; }) inputs;
-}
+    nix.nixPath = lib.mapAttrsToList (n: _: "${n}=/etc/nix/inputs/${n}") (self.lib.inputsWithPkgs inputs);
+    nix.registry = builtins.mapAttrs (_: v: { flake = v; }) (self.lib.inputsWithOutputs inputs);
+    environment.etc = lib.mapAttrs' (n: v: self.lib.nameValuePair "nix/inputs/${n}" { source = v.outPath; }) inputs;
+  }
+  (lib.mkIf pkgs.stdenv.isDarwin {
+    services.nix-daemon.enable = true;
+    system.stateVersion = 4;
+  })
+]
