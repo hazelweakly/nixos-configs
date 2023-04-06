ef7acacf84d4fedbcc952914e58fb12f89947952
Hazel Weakly <hazel@theweaklys.com>
2023-04-05 21:44:15 -0700

    hideous fuckin crimes to build tree-sitter because i am a slut for neovim-nightly

--- overlays/neovim-nightly.nix
+++ overlays/neovim-nightly.nix
@@ -1,5 +1,5 @@
 final: prev: rec {
-  neovim-unwrapped = neovim-nightly; # Needed so that neovim-remote picks up the right neovim
+  neovim-unwrapped = neovim-nightly.override { tree-sitter = final.tree-sitter; }; # Needed so that neovim-remote picks up the right neovim
   neovim-nightly = final.inputs.neovim-flake.packages.${prev.system}.neovim;
   neovim-remote = prev.neovim-remote.overridePythonAttrs (o: { doCheck = false; });
 }
