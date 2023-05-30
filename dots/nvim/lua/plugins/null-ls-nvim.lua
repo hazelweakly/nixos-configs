2a803b45e2be54dea0517a26c08d493643aa12e1
Hazel Weakly <hazel@theweaklys.com>
2023-05-30 14:04:18 -0700

    cleanup null-ls. figure out why prettier thingy is breaking later.

--- dots/nvim/lua/plugins/null-ls-nvim.lua
+++ dots/nvim/lua/plugins/null-ls-nvim.lua
@@ -11,27 +11,16 @@ return {
 
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.diagnostics.actionlint.with({
-          runtime_condition = function(params)
-            return string.match(params.bufname, ".*%.github/workflows/.*%.ya?ml") ~= nil
-          end,
-          extra_args = function(_)
-            local path = require("null-ls.utils").path.join(".github", "actionlint.yaml")
-            local has = require("null-ls.utils").make_conditional_utils().has_file(path)
-            if has then
-              return { "-config-file", path }
-            else
-              return false
-            end
-          end,
-        }),
+        null_ls.builtins.diagnostics.actionlint,
 
         null_ls.builtins.formatting.shfmt.with({
           extra_args = function(params)
+            ---@diagnostic disable-next-line: redundant-parameter
             return { "-s", "-i", vim.api.nvim_buf_get_option(params.bufnr, "shiftwidth") }
           end,
         }),
-        null_ls.builtins.formatting.prettier_d_slim, -- breaks formatexpr for gq
+        -- null_ls.builtins.formatting.prettier_d_slim, -- breaks formatexpr for gq
+        null_ls.builtins.formatting.prettier, -- breaks formatexpr for gq
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.terraform_fmt,
         null_ls.builtins.formatting.shellharden,
