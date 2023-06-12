0e3f554db83730201f6003090c04abc99e0965c5
Hazel Weakly <hazel@theweaklys.com>
2023-06-12 10:19:29 -0700

    dont move cursor in autolist

--- dots/nvim/lua/plugins/autolist-nvim.lua
+++ dots/nvim/lua/plugins/autolist-nvim.lua
@@ -18,7 +18,13 @@ return {
     vim.api.nvim_create_autocmd("TextChanged", {
       callback = function()
         if vim.tbl_contains({ "d", "<", ">" }, vim.v.operator) then
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
           vim.cmd.normal({ autolist.force_recalculate(nil, nil), bang = false })
+          local lastline = vim.fn.line("$")
+          if line > lastline then
+            line = lastline
+          end
+          vim.api.nvim_win_set_cursor(0, { line, col })
         end
       end,
     })
