50762e60f146d66355c2e38f03e21106e21ebb42
Hazel Weakly <hazel@theweaklys.com>
2023-10-30 17:54:40 -0500

    update plugins

--- dots/nvim/lua/plugins/tokyonight-nvim.lua
+++ dots/nvim/lua/plugins/tokyonight-nvim.lua
@@ -2,7 +2,8 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
-    vim.api.nvim_create_autocmd({ "User VeryLazy" }, {
+    vim.api.nvim_create_autocmd({ "User" }, {
+      pattern = "VeryLazy",
       callback = function()
         require("configs.colors").setup()
       end,
