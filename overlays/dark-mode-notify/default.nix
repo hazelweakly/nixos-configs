a61bda0b13a3419dd18d9d22be33f36443ba29c3
Hazel Weakly <hazel@theweaklys.com>
2023-12-21 10:51:52 -0800

    fix dark mode notify

--- overlays/dark-mode-notify/default.nix
+++ overlays/dark-mode-notify/default.nix
@@ -1,3 +1,22 @@
 final: prev: {
-  dark-mode-notify = prev.writeScriptBin "dark-mode-notify" (builtins.readFile ./dark-mode-notify.swift);
+  dark-mode-notify =
+    let
+      writeSwift = name: { swiftc ? "/usr"
+                         , swiftcArgs ? [ ]
+                         , strip ? false
+                         }: prev.writers.makeBinWriter
+        {
+          compileScript = ''
+            cp $contentPath tmp.swift
+            ${swiftc}/bin/swiftc ${prev.lib.escapeShellArgs swiftcArgs} tmp.swift
+            mv tmp $out
+          '';
+          inherit strip;
+        }
+        name;
+
+      writeSwiftBin = name: writeSwift "/bin/${name}";
+    in
+
+    writeSwiftBin "dark-mode-notify" { } (builtins.readFile ./dark-mode-notify.swift);
 }
